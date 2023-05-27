6592327877e818530256d9de32edd4edc9a63830
Lasse Mikkonen <lasse.mikkonen@gmail.com>
2023-05-27 23:30:54 +0300

    Update excel2txt.py
    Added input for excel file name

--- excel2txt.py
+++ excel2txt.py
@@ -1,10 +1,20 @@
 import pandas as pd
+import argparse
 
-# read the excel file
-df = pd.read_excel('ekkeli.xlsx')
+# Create the parser
+parser = argparse.ArgumentParser(description='Split Excel file into text files')
 
-# iterate over the rows
+# Add an argument
+parser.add_argument('file', type=str, help='The Excel file to split')
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Read the excel file
+df = pd.read_excel(args.file)
+
+# Iterate over the rows
 for index, row in df.iterrows():
-    # open a file with name from column 'A' and write column 'B' into it
-    with open(str(row['File']) + '.txt', 'w') as f:
-        f.write(str(row['Content']))
+    # Open a file with name from column 'A' and write column 'B' into it
+    with open(str(row['A']) + '.txt', 'w') as f:
+        f.write(str(row['B']))
